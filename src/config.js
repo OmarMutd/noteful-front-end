0218b901879051814c9e9eb6820df5f88ec6ac35
Omar Muhanna <omarmuhomar@hotmail.com>
2020-03-13 15:12:39 -0500

    ...

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  // API_ENDPOINT: 'https://fathomless-shelf-77351.herokuapp.com/api',
-  API_ENDPOINT: 'https://dashboard.heroku.com/apps/glacial-shelf-49987/api',
+  API_ENDPOINT: 'https://fathomless-shelf-77351.herokuapp.com/api',
+  // API_ENDPOINT: 'https://dashboard.heroku.com/apps/glacial-shelf-49987/api',
   // API_ENDPOINT: 'http://localhost:8000/api',
   API_TOKEN: process.env.API_TOKEN || '0000'
 }
